38ab7224b901cea4ab749d9f85324ce2768cfc46
João Bortolace <54319789+JoaoBortolace@users.noreply.github.com>
2024-10-27 12:53:25 -0300

    Feito o teclado

--- Base/main.cpp
+++ Base/main.cpp
@@ -17,6 +17,7 @@
 
 /* -------- Variáveis Globais -------- */
 static Raspberry::Teclado comando = Raspberry::Teclado::NAO_SELECIONADO;
+static Mat_<Raspberry::Cor> teclado(TECLADO_WIDTH, TECLADO_HEIGHT, Paleta::blue01);
 
 /* -------- Callbacks -------- */
 void mouse_callback(int event, int x, int y, int flags, void *usedata)
@@ -36,12 +37,15 @@ void mouse_callback(int event, int x, int y, int flags, void *usedata)
                 switch (row) {
                     case 0:
                         comando = Teclado::DIAGONAL_FRENTE_ESQUERDA;
+                        arrowedLine(teclado, Point(60, 60), Point(20, 20), Paleta::red, 2.5);
                         break;
                     case 1:
                         comando = Teclado::GIRA_ESQUERDA;
+                        arrowedLine(teclado, Point(60, 120), Point(20, 120), Paleta::red, 3);
                         break;
                     case 2:
                         comando = Teclado::DIAGONAL_ATRAS_ESQUERDA;
+                        arrowedLine(teclado, Point(60, 180), Point(20, 220), Paleta::red, 2.5);    
                         break;
                 }
                 break;
@@ -49,12 +53,15 @@ void mouse_callback(int event, int x, int y, int flags, void *usedata)
                 switch (row) {
                     case 0:
                         comando = Teclado::FRENTE;
+                        arrowedLine(teclado, Point(120, 60), Point(120, 20), Paleta::red, 3);
                         break;
                     case 1:
                         comando = Teclado::NAO_FAZ_NADA;
+                        rectangle(teclado, Point(115, 115), Point(125, 125), Paleta::grey, -1);
                         break;
                     case 2:
                         comando = Teclado::ATRAS;
+                        arrowedLine(teclado, Point(120, 180), Point(120, 220), Paleta::red, 3);
                         break;
                 }
                 break;
@@ -62,12 +69,15 @@ void mouse_callback(int event, int x, int y, int flags, void *usedata)
                 switch (row) {
                     case 0:
                         comando = Teclado::DIAGONAL_FRENTE_DIREITA;
+                        arrowedLine(teclado, Point(180, 60), Point(220, 20), Paleta::red, 2.5);
                         break;
                     case 1:
                         comando = Teclado::GIRA_DIREITA;
+                        arrowedLine(teclado, Point(180, 120), Point(220, 120), Paleta::red, 3);
                         break;
                     case 2:
                         comando = Teclado::DIAGONAL_ATRAS_DIREITA;
+                        arrowedLine(teclado, Point(180, 180), Point(220, 220), Paleta::red, 2.5);
                         break;
                 }
                 break;
@@ -78,6 +88,15 @@ void mouse_callback(int event, int x, int y, int flags, void *usedata)
     }
     else if (event == EVENT_LBUTTONUP) {
         comando = Raspberry::Teclado::NAO_SELECIONADO;
+        arrowedLine(teclado, Point(120, 60), Point(120, 20), Paleta::blue03, 3);
+        arrowedLine(teclado, Point(120, 180), Point(120, 220), Paleta::blue03, 3);
+        arrowedLine(teclado, Point(60, 120), Point(20, 120), Paleta::blue03, 3);
+        arrowedLine(teclado, Point(180, 120), Point(220, 120), Paleta::blue03, 3);
+        arrowedLine(teclado, Point(60, 60), Point(20, 20), Paleta::blue03, 2.5);
+        arrowedLine(teclado, Point(60, 180), Point(20, 220), Paleta::blue03, 2.5);    
+        arrowedLine(teclado, Point(180, 60), Point(220, 20), Paleta::blue03, 2.5);
+        arrowedLine(teclado, Point(180, 180), Point(220, 220), Paleta::blue03, 2.5);
+        rectangle(teclado, Point(115, 115), Point(125, 125), Paleta::blue03, -1);
     }
 }
 
@@ -92,17 +111,21 @@ int main(int argc, char *argv[])
     namedWindow("RaspCam", WINDOW_AUTOSIZE);
     setMouseCallback("RaspCam", mouse_callback);
     Mat_<Raspberry::Cor> frameBuf;
-    
-    // Teclado Virtual
-    Mat_<Raspberry::Cor> teclado(TECLADO_WIDTH, TECLADO_HEIGHT, Paleta::blue01);
-    
-    line(teclado, Point(0, 80), Point(240, 80), Paleta::grey, 2);
-    line(teclado, Point(0, 160), Point(240, 160), Paleta::grey, 2);    
-    line(teclado, Point(80, 0), Point(80, 240), Paleta::grey, 2);    
-    line(teclado, Point(160, 0), Point(160, 240), Paleta::grey, 2);    
-
+        
+    line(teclado, Point(0, 80), Point(240, 80), Paleta::blue02, 2.5);
+    line(teclado, Point(0, 160), Point(240, 160), Paleta::blue02, 2.5);    
+    line(teclado, Point(80, 0), Point(80, 240), Paleta::blue02, 2.5);    
+    line(teclado, Point(160, 0), Point(160, 240), Paleta::blue02, 2.5);  
+    rectangle(teclado, Point(115, 115), Point(125, 125), Paleta::blue03, -1);
     arrowedLine(teclado, Point(120, 60), Point(120, 20), Paleta::blue03, 3);
-    
+    arrowedLine(teclado, Point(120, 180), Point(120, 220), Paleta::blue03, 3);
+    arrowedLine(teclado, Point(60, 120), Point(20, 120), Paleta::blue03, 3);
+    arrowedLine(teclado, Point(180, 120), Point(220, 120), Paleta::blue03, 3);
+    arrowedLine(teclado, Point(60, 60), Point(20, 20), Paleta::blue03, 2.5);
+    arrowedLine(teclado, Point(60, 180), Point(20, 220), Paleta::blue03, 2.5);    
+    arrowedLine(teclado, Point(180, 60), Point(220, 20), Paleta::blue03, 2.5);
+    arrowedLine(teclado, Point(180, 180), Point(220, 220), Paleta::blue03, 2.5);
+
     try {
         // Conecta à Raspberry
         Client client(argv[1], argv[2]);
